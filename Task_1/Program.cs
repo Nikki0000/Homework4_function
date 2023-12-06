b3e21d9c75f66f25fbe0947a2f37978be3ac5036
Savodyarkin Nikita <snikitafut@mail.ru>
2023-12-06 12:12:17 +0300

    endless request for numbers

---
+++ Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+
+bool IsSumOfDigitEven(int num) {
+    int sum = 0;
+    while (num != 0) {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    return sum % 2 == 0;
+}
+
+
+
+while (true) {
+    Console.WriteLine("Введите целое число");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "q") {
+        Console.WriteLine("Программа завершена");
+        break;
+    }
+
+    if(int.TryParse(input, out int number)) {
+        if (IsSumOfDigitEven(number)) {
+            Console.WriteLine($"Сумма цифр числа {number} четная. Программа завершена");
+            break;
+        }
+    } else {
+        Console.WriteLine("Не корректный ввод");
+    }
+
+}
